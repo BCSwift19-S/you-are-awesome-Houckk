dd7a64ada86ab7307193256b2053867626a7e47c
Kenyan Houck <kenyan@macbook.local>
2019-01-21 19:37:06 -0500

    2.12 Complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
 
     //Code below executes when the app's view first loads
     override func viewDidLoad() {
@@ -17,20 +18,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMessagePressed1(_ sender: UIButton) {
-        let message1 = "You are Awesome!"
-        let message2 = "You are Great!"
-        let message3 = "You are Amazing!"
         
-        if (messageLabel.text == message1)
-        {
-            messageLabel.text = message2
-        } else if (messageLabel.text == message2)
-        {
-            messageLabel.text = message3
-        } else{
-            messageLabel.text = message1
-        }
-        messageLabel.textColor = UIColor.red
+        let messages = ["You are Awesome!", "You are Great!", "You are Fantastic!", "When the Genius Bar needs help, they call you", "You brighten my day!", "You are da bomb!", "Hey, fabulous!", "You are tremendous!", "You've got the design skills of Jony Ive!", "I can't wait to download your app!"]
+        
+        
+        
+        
+        messageLabel.text = messages.randomElement()!
+        
+//        messageLabel.text = messages[index]
+//
+//        if (index == messages.count-1)
+//        {
+//            index = 0
+//        } else {
+//            index = index + 1
+//        }
+//        messageLabel.textColor = UIColor.red
     }
     
 }
